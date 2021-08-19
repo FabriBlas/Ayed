026586c2af6f73f4ee2cac94b82d699f2c32ac93
Diane <diane.fernandez2015@gmail.com>
2021-08-18 22:15:35 -0300

    is winner listo

--- eclipse-workspace/Tateti/src/a/Tateti.java
+++ eclipse-workspace/Tateti/src/a/Tateti.java
@@ -1,15 +1,16 @@
 package a;
 
-import java.util.Iterator;
-
+//import java.util.Iterator;
 public class Tateti {
 	
+   // char vacio= '-';
 	char arreglo[][];
 	
 	public Tateti(char[][] arreglo) {
-		super();
+		
 		this.arreglo = arreglo;
 	}
+	public Tateti () {}
 
 	void print_board( ){
 		for (int i = 0; i < this.arreglo.length; i++) {
@@ -21,20 +22,56 @@ public class Tateti {
 		}
 	}
 	
+	
+	
 	// Conocer si un jugador gano
-	boolean is_winner( char player) {
-		return false;
+	boolean is_winner(char arreglo[][]) {
+		
+		      if  (arreglo[0][0]=='x' && arreglo[0][1]=='x' && arreglo[0][2]=='x' 
+				|| arreglo[1][0]=='x' && arreglo[1][1]=='x' && arreglo[1][2]=='x'
+				|| arreglo[2][0]=='x' && arreglo[2][1]=='x' && arreglo[2][2]=='x'
+				|| arreglo[0][0]=='x' && arreglo[1][0]=='x' && arreglo[2][0]=='x'
+				|| arreglo[0][1]=='x' && arreglo[1][1]=='x' && arreglo[2][1]=='x'
+				|| arreglo[0][2]=='x' && arreglo[1][2]=='x' && arreglo[2][2]=='x'
+				|| arreglo[0][0]=='x' && arreglo[1][1]=='x' && arreglo[2][2]=='x'
+				|| arreglo[0][2]=='x' && arreglo[1][1]=='x' && arreglo[2][0]=='x') {
+			return true;
+			
+		}else if  (arreglo[0][0]=='o' && arreglo[0][1]=='o' && arreglo[0][2]=='o' 
+				|| arreglo[1][0]=='o' && arreglo[1][1]=='o' && arreglo[1][2]=='o'
+				|| arreglo[2][0]=='o' && arreglo[2][1]=='o' && arreglo[2][2]=='o'
+				|| arreglo[0][0]=='o' && arreglo[1][0]=='o' && arreglo[2][0]=='o'
+				|| arreglo[0][1]=='o' && arreglo[1][1]=='o' && arreglo[2][1]=='o'
+				|| arreglo[0][2]=='o' && arreglo[1][2]=='o' && arreglo[2][2]=='o'
+				|| arreglo[0][0]=='o' && arreglo[1][1]=='o' && arreglo[2][2]=='o'
+				|| arreglo[0][2]=='o' && arreglo[1][1]=='o' && arreglo[2][0]=='o') {
+			return true;
+			 }
+		      else {
+			return false;
+		}
+	
 	}
+	
+	
+	
+	
+	
 	
 	// Insertar ficha en el tablero
-	void player_turn (char player, int pos) {
-		
+	void player_turn (char player, int pos, char arreglo[][]) {
+
+	
 	}
+	
+	
 	
 	// sabes si hay un espacio libre
-	boolean has_cell_free() {
-		return true;
-	}
+	boolean has_cell_free(char player, int pos) {
+		
+	   
+	    return true;
 	
-
+	
+	}
 }
--- eclipse-workspace/Tateti/src/a/main.java
+++ eclipse-workspace/Tateti/src/a/main.java
@@ -1,15 +1,41 @@
 package a;
 
+//import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
+		//Scanner teclado= new Scanner (System.in);
 		
 		char tablero[][] = {{'-','-','-'},
 							{'-','-','-'},
 							{'-','-','-'}};
 		
 		Tateti ttt = new Tateti(tablero);
+		
 		ttt.print_board();
+		
+	
+		
+		Tateti ganador= new Tateti ();
+		System.out.println(ganador.is_winner(tablero));
+		ganador.is_winner(tablero);
+		
+	
+		
+		
+		
+	
+		
+		
+		
+	 
+		
+		
+
+	
+		
+		
 
 	}
 
